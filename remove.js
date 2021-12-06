674d0f2d445212d933123e8ec3d67d9ec71ec504
zhangjie158 <zhangjie158@meituan.com>
2021-12-06 20:05:42 +0800

    fix

--- remove.js
+++ remove.js
@@ -1,22 +1,16 @@
-const findVideo = (item) => {
-    if(!item) return false
-    if(typeof item.className == 'string' && (item.className.includes('ZVideoItem') || item.className.includes('VideoAnswerPlayer'))) return true
-    const children = Array.from(item.children)
-    if(children.length > 0) {
-        for(let child of children) {
-            if(findVideo(child)) return true
-        }
-    }
-    return false
+const names = ['ZVideoItem','VideoAnswerPlayer','Pc-feedAd-container']
+
+const hasNames = (str) => {
+    return names.some((item) => str.includes(item))
 }
 
-const findAd = (item) => {
+const findNames = item => {
     if(!item) return false
-    if(typeof item.className == 'string' && item.className.includes('Pc-feedAd-container ')) return true
+    if(typeof item.className == 'string' && hasNames(item.className)) return true
     const children = Array.from(item.children)
     if(children.length > 0) {
         for(let child of children) {
-            if(findAd(child)) return true
+            if(findNames(child)) return true
         }
     }
     return false
@@ -27,7 +21,7 @@ const run = () => {
     const cardArr = Array.from(cardElements)
 
     cardArr.forEach(item => {
-        if(findVideo(item) || findAd(item)) {
+        if(findNames(item)) {
             item.style.display = 'none'
         }
     })
